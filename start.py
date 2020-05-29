ac07f478c79a6de05b9c4e8792728ccceb6390ec
Michael <22685648+MichaelRP1@users.noreply.github.com>
2020-05-29 15:58:42 -0400

    Removing App ID

--- start.py
+++ start.py
@@ -1,8 +1,10 @@
 import requests
+import json
 
 # Get API Token
-oauthtask = {"response_type": "code", "client_id": "a15b0252-1b04-4502-8bce-b382318b0f6c", "scope": "read"}
-oauthresp = requests.get('developer.blackboard.com/learn/api/public/v1/oauth2/authorizationcode', json=oauthtask)
-if oauthresp.status_code != 200:
-    print('Error {}'.format(oauthresp.status_code))
-print('Created task. ID: {}'.format(oauthresp.json()["id"]))
+def auth_code():
+    oauthtask = {"response_type": "code", "client_id": "", "scope": "read"}
+    oauthresp = requests.get('developer.blackboard.com/learn/api/public/v1/oauth2/authorizationcode', json=oauthtask)
+    if oauthresp.status_code != 200:
+        print('Error {}'.format(oauthresp.status_code))
+    print('Created task. ID: {}'.format(oauthresp.json()["id"]))
