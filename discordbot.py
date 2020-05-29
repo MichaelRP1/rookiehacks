a331680dafe113aa5f99b9cc2704cebd607617cc
Michael <22685648+MichaelRP1@users.noreply.github.com>
2020-05-29 17:35:23 -0400

    Starting Geocoding using nominatim

---
+++ discordbot.py
@@ -0,0 +1,29 @@
+import discord
+import json
+from geocode.py import geocode
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged on as {0.user}!'.format(client))
+    print(f'In {len(client.guilds)} guilds!')
+    await client.change_presence(activity=discord.Game(name='MLH RookieHacks'))
+
+@client.event    
+async def on_message(message):
+    print(message.content)
+    if message.content.startswith("!location"):
+        client_location = message.content[len("!location") +1 :]
+        location = geolocation(client_location)
+        channel = client.get_channel(message.channel.id)
+        await channel.send(location)
+    if message.content.startswith("!shutdown"):
+        await client.logout()
+
+token = ""
+with open('creds.json') as json_file:
+    data = json.load(json_file)
+    token = data['token']
+
+client.run(token)
--- geocode.py
+++ geocode.py
@@ -1,29 +1,6 @@
-import discord
 import json
+import discord
+import requests
 
-client_location = ""
-
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('Logged on as {0.user}!'.format(client))
-    print(f'In {len(client.guilds)} guilds!')
-    await client.change_presence(activity=discord.Game(name='MLH RookieHacks'))
-
-@client.event    
-async def on_message(message):
-    print(message.content)
-    if message.content.startswith("!location"):
-        client_location = message.content[len("!location") +1 :]
-        channel = client.get_channel(message.channel.id)
-        await channel.send(client_location)
-    if message.content.startswith("!shutdown"):
-        await client.logout()
-
-token = ""
-with open('creds.json') as json_file:
-    data = json.load(json_file)
-    token = data['token']
-
-client.run(token)
+def geocode(client_location):
+    location = requests.get("https://nominatim.openstreetmap.org/search/{0}?format=json&accept-language=en-US&countrycodes=us&limit=1".format(client_location))
